560e3c0d05b0ae0110529dec017edcc9b3bc6d8b
joohee <2170095@ewhain.net>
2022-11-16 12:44:49 +0900

    pop up background 수정

--- JS/sign_up.js
+++ JS/sign_up.js
@@ -1,7 +1,16 @@
 let back = document.querySelector(".img-cover");
+let content = document.querySelector(".signUpInput");
 let pop = document.querySelector("#pop");
+let nav = document.querySelector(".nav");
 
 function popup(){
     pop.style.display = 'block';
-    back.setAttribute('z-index', 2);
+    back.style.zIndex = '2';
+    content.style.zIndex = '1';
+}
+
+function delete_box(){
+    pop.style.display = 'none';
+    back.style.zIndex = '1';
+    content.style.zIndex = '2';
 }
